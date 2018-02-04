1bcaf65ba664e87fc2c373a14a302390b8efbb38
ashinzekene <ashinzekene@gmail.com>
2018-02-04 23:34:18 +0100

    refactored game_functions.check_events

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -7,20 +7,32 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key ==  pygame.K_RIGHT:
-                ship.moving_right = True
-            if event.key ==  pygame.K_LEFT:
-                ship.moving_left = True
-            if event.key ==  pygame.K_UP:
-                ship.moving_up = True
-            if event.key ==  pygame.K_DOWN:
-                ship.moving_down = True
+            check_events_key_down(event, ship)
         elif event.type == pygame.KEYUP:
-            ship.moving_right = False
-            ship.moving_left = False
-            ship.moving_up = False
-            ship.moving_down = False
+            check_events_key_up(event, ship)
                 
+def check_events_key_up(event, ship):
+    # Responds to key releases
+    if event.key ==  pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key ==  pygame.K_LEFT:
+        ship.moving_left = False
+    if event.key ==  pygame.K_UP:
+        ship.moving_up = False
+    if event.key ==  pygame.K_DOWN:
+        ship.moving_down = False  
+    
+def check_events_key_down(event, ship):
+    # Responds to key releases
+    if event.key ==  pygame.K_RIGHT:
+        ship.moving_right = True
+    if event.key ==  pygame.K_LEFT:
+        ship.moving_left = True
+    if event.key ==  pygame.K_UP:
+        ship.moving_up = True
+    if event.key ==  pygame.K_DOWN:
+        ship.moving_down = True  
+    
 def update_screen(ai_settings, screen, ship):
     """Updates image son the screen and flips to the next screen"""
     screen.fill(ai_settings.bg_color)
