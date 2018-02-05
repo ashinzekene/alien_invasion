a0a244f0ade0d9eec559a6268ae4931edd4fb44d
ashinzekene <ashinzekene@gmail.com>
2018-02-05 05:59:49 +0100

    making bullets

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,3 +5,8 @@ class Settings():
         self.screen_height = 640
         self.bg_color = (230, 230, 230)      
         self.ship_speed_factor = 1.5
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
